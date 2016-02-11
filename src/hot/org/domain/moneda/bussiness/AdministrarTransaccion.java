bd8840abeb13b11a82318d66cb794a66dd7b8cdf
lfernandortiz <lfernandortiz@gmail.com>
2016-02-11 16:04:13 -0500

    Eliminacion de metodo

--- src/hot/org/domain/moneda/bussiness/AdministrarTransaccion.java
+++ src/hot/org/domain/moneda/bussiness/AdministrarTransaccion.java
@@ -2765,13 +2765,6 @@ public class AdministrarTransaccion
 	}//fin del parche
 	
 	
-	public float round(float d, int decimalPlace) {
-        BigDecimal bd = new BigDecimal(Float.toString(d));
-        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
-        return bd.floatValue();
-    }
-	
-	
 }// fin de la clase AdministrarTransaccion
 
 
